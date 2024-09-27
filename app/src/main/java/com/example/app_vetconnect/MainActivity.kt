2e471b41313a1ef3112cc079813b4cd788d10e81
HamesEl <u202212684@upc.edu.pe>
2024-09-26 23:56:37 -0500

    Feat(main): Add main view

--- app/src/main/java/com/example/app_vetconnect/MainActivity.kt
+++ app/src/main/java/com/example/app_vetconnect/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.app_vetconnect
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +20,17 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val btCreateAccount = findViewById<Button>(R.id.btCreateAccount)
+        btCreateAccount.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        val tvLogin = findViewById<TextView>(R.id.tvLogin)
+        tvLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
